923044606d53c91067424ecb2f33dbfbb46afda2
fatevil <marekoka@seznam.cz>
2016-05-18 19:59:36 +0200

    Reservation update GUI now works.

--- src/main/java/gui/user/ReservationManagementPanel.java
+++ src/main/java/gui/user/ReservationManagementPanel.java
@@ -7,6 +7,7 @@ import gui.ManagedCard;
 import gui.customcomponents.CardChoosingButton;
 import model.Reservation;
 import utility.Authorization;
+import utility.SpringUtilities;
 
 import javax.swing.*;
 import java.awt.*;
@@ -20,7 +21,13 @@ public class ReservationManagementPanel extends ManagedCard {
     private JButton previousButton = new JButton("Previous");
 
     private JButton menuButton = new CardChoosingButton("Menu", UserGuidepostPanel.class);
-    private JButton makeReservationButton = new JButton("Make reservation");
+    private JButton makeReservationButton = new JButton("Back");
+
+
+    private boolean state = false;
+    private JPanel cards = new JPanel(new CardLayout());
+    private CardLayout cardLayout;
+
 
     {
         setupComponents();
@@ -41,9 +48,11 @@ public class ReservationManagementPanel extends ManagedCard {
         RoomDAOImpl roomDAO = new RoomDAOImpl();
         RezervationDAOImp rezervationDAOImp = new RezervationDAOImp();
 
-
         final JPanel bottomPanel = bottomMenu();
         JPanel topPanel = upperMenu();
+
+        UpdateReservationPanel temp1;
+
 
         JPanel panel = new JPanel(new SpringLayout());
         JPanel centerPanel = new JPanel(null);
@@ -72,6 +81,9 @@ public class ReservationManagementPanel extends ManagedCard {
             day = r.getToDate().get(Calendar.DAY_OF_MONTH);
             panel.add(new JLabel((String.format("%d/%d/%d", day, month, year))));
 
+            cards.add(new UpdateReservationPanel(r), r.getId().toString());
+
+
             button = new CardChoosingButton("DELETE", ReservationManagementPanel.class, true);
             button.addActionListener(e -> {
                 Authorization.getUser().getReservations().remove(r);
@@ -79,15 +91,17 @@ public class ReservationManagementPanel extends ManagedCard {
             });
             panel.add(button);
 
-            button = new CardChoosingButton("UPDATE", ReservationManagementPanel.class, true);
-            button.addActionListener(e -> {
-                //Authorization.getUser().getReservations().remove(r);
-                new UserDAOImpl().update(Authorization.getUser());
+            JButton button1 = new JButton("UPDATE");
+            button1.addActionListener(e -> {
+                cardLayout.show(cards, r.getId().toString());
+
+                nextButton.setEnabled(false);
+                previousButton.setEnabled(false);
+                makeReservationButton.setEnabled(true);
             });
-            panel.add(button);
+            panel.add(button1);
 
         }
-        // Lay out the panel.
         SpringUtilities.makeGrid(panel, Authorization.getUser().getReservations().size() + 1, 6, // rows, cols
                 50, 0, // initialX, initialY
                 15, 15);// xPad, yPad
@@ -97,19 +111,26 @@ public class ReservationManagementPanel extends ManagedCard {
         scrollPane.setBounds(10, 10, 960, 325);
         scrollPane.getVerticalScrollBar().setUnitIncrement(16);
 
-
-        for (Reservation r : Authorization.getUser().getReservations()) {
-            //          centerPanel.add(new JLabel("# " + r.getRoom().getId()));
-            //centerPanel.add(new JLabel("# " + r.getFromDate()));
-            //centerPanel.add(new JLabel("# " + r.getToDate()));
-            //        centerPanel.add(new JButton("delete"));
-
-        }
-
         centerPanel.setPreferredSize(new Dimension(500, 400));
         centerPanel.add(scrollPane);
-        //centerPanel.add(panel);
-        add(centerPanel, BorderLayout.CENTER);
+
+        makeReservationButton.setEnabled(false);
+        makeReservationButton.addActionListener(e -> {
+            cardLayout.show(cards, "management");
+            makeReservationButton.setEnabled(false);
+
+            nextButton.setEnabled(true);
+            previousButton.setEnabled(true);
+        });
+
+
+        cards.add(centerPanel, "management");
+
+        cardLayout = (CardLayout) cards.getLayout();
+
+
+        add(cards, BorderLayout.CENTER);
+        cardLayout.show(cards, "management");
     }
 
     private JPanel upperMenu() {
@@ -136,161 +157,5 @@ public class ReservationManagementPanel extends ManagedCard {
         return bottomPanel;
     }
 
-    private static class SpringUtilities {
-        /**
-         * A debugging utility that prints to stdout the component's minimum,
-         * preferred, and maximum sizes.
-         */
-        public static void printSizes(Component c) {
-            System.out.println("minimumSize = " + c.getMinimumSize());
-            System.out.println("preferredSize = " + c.getPreferredSize());
-            System.out.println("maximumSize = " + c.getMaximumSize());
-        }
 
-        /**
-         * Aligns the first <code>rows</code> * <code>cols</code> components of
-         * <code>parent</code> in a grid. Each component is as big as the maximum
-         * preferred width and height of the components. The parent is made just big
-         * enough to fit them all.
-         *
-         * @param rows     number of rows
-         * @param cols     number of columns
-         * @param initialX x location to start the grid at
-         * @param initialY y location to start the grid at
-         * @param xPad     x padding between cells
-         * @param yPad     y padding between cells
-         */
-        public static void makeGrid(Container parent, int rows, int cols, int initialX, int initialY,
-                                    int xPad, int yPad) {
-            SpringLayout layout;
-            try {
-                layout = (SpringLayout) parent.getLayout();
-            } catch (ClassCastException exc) {
-                System.err.println("The first argument to makeGrid must use SpringLayout.");
-                return;
-            }
-
-            Spring xPadSpring = Spring.constant(xPad);
-            Spring yPadSpring = Spring.constant(yPad);
-            Spring initialXSpring = Spring.constant(initialX);
-            Spring initialYSpring = Spring.constant(initialY);
-            int max = rows * cols;
-
-            // Calculate Springs that are the max of the width/height so that all
-            // cells have the same size.
-            Spring maxWidthSpring = layout.getConstraints(parent.getComponent(0)).getWidth();
-            Spring maxHeightSpring = layout.getConstraints(parent.getComponent(0)).getWidth();
-            for (int i = 1; i < max; i++) {
-                SpringLayout.Constraints cons = layout.getConstraints(parent.getComponent(i));
-
-                maxWidthSpring = Spring.max(maxWidthSpring, cons.getWidth());
-                maxHeightSpring = Spring.max(maxHeightSpring, cons.getHeight());
-            }
-
-            // Apply the new width/height Spring. This forces all the
-            // components to have the same size.
-            for (int i = 0; i < max; i++) {
-                SpringLayout.Constraints cons = layout.getConstraints(parent.getComponent(i));
-
-                cons.setWidth(maxWidthSpring);
-                cons.setHeight(maxHeightSpring);
-            }
-
-            // Then adjust the x/y constraints of all the cells so that they
-            // are aligned in a grid.
-            SpringLayout.Constraints lastCons = null;
-            SpringLayout.Constraints lastRowCons = null;
-            for (int i = 0; i < max; i++) {
-                SpringLayout.Constraints cons = layout.getConstraints(parent.getComponent(i));
-                if (i % cols == 0) { // start of new row
-                    lastRowCons = lastCons;
-                    cons.setX(initialXSpring);
-                } else { // x position depends on previous component
-                    cons.setX(Spring.sum(lastCons.getConstraint(SpringLayout.EAST), xPadSpring));
-                }
-
-                if (i / cols == 0) { // first row
-                    cons.setY(initialYSpring);
-                } else { // y position depends on previous row
-                    cons.setY(Spring.sum(lastRowCons.getConstraint(SpringLayout.SOUTH), yPadSpring));
-                }
-                lastCons = cons;
-            }
-
-            // Set the parent's size.
-            SpringLayout.Constraints pCons = layout.getConstraints(parent);
-            pCons.setConstraint(SpringLayout.SOUTH, Spring.sum(Spring.constant(yPad), lastCons
-                    .getConstraint(SpringLayout.SOUTH)));
-            pCons.setConstraint(SpringLayout.EAST, Spring.sum(Spring.constant(xPad), lastCons
-                    .getConstraint(SpringLayout.EAST)));
-        }
-
-        /* Used by makeCompactGrid. */
-        private static SpringLayout.Constraints getConstraintsForCell(int row, int col, Container parent,
-                                                                      int cols) {
-            SpringLayout layout = (SpringLayout) parent.getLayout();
-            Component c = parent.getComponent(row * cols + col);
-            return layout.getConstraints(c);
-        }
-
-        /**
-         * Aligns the first <code>rows</code> * <code>cols</code> components of
-         * <code>parent</code> in a grid. Each component in a column is as wide as
-         * the maximum preferred width of the components in that column; height is
-         * similarly determined for each row. The parent is made just big enough to
-         * fit them all.
-         *
-         * @param rows     number of rows
-         * @param cols     number of columns
-         * @param initialX x location to start the grid at
-         * @param initialY y location to start the grid at
-         * @param xPad     x padding between cells
-         * @param yPad     y padding between cells
-         */
-        public static void makeCompactGrid(Container parent, int rows, int cols, int initialX,
-                                           int initialY, int xPad, int yPad) {
-            SpringLayout layout;
-            try {
-                layout = (SpringLayout) parent.getLayout();
-            } catch (ClassCastException exc) {
-                System.err.println("The first argument to makeCompactGrid must use SpringLayout.");
-                return;
-            }
-
-            // Align all cells in each column and make them the same width.
-            Spring x = Spring.constant(initialX);
-            for (int c = 0; c < cols; c++) {
-                Spring width = Spring.constant(0);
-                for (int r = 0; r < rows; r++) {
-                    width = Spring.max(width, getConstraintsForCell(r, c, parent, cols).getWidth());
-                }
-                for (int r = 0; r < rows; r++) {
-                    SpringLayout.Constraints constraints = getConstraintsForCell(r, c, parent, cols);
-                    constraints.setX(x);
-                    constraints.setWidth(width);
-                }
-                x = Spring.sum(x, Spring.sum(width, Spring.constant(xPad)));
-            }
-
-            // Align all cells in each row and make them the same height.
-            Spring y = Spring.constant(initialY);
-            for (int r = 0; r < rows; r++) {
-                Spring height = Spring.constant(0);
-                for (int c = 0; c < cols; c++) {
-                    height = Spring.max(height, getConstraintsForCell(r, c, parent, cols).getHeight());
-                }
-                for (int c = 0; c < cols; c++) {
-                    SpringLayout.Constraints constraints = getConstraintsForCell(r, c, parent, cols);
-                    constraints.setY(y);
-                    constraints.setHeight(height);
-                }
-                y = Spring.sum(y, Spring.sum(height, Spring.constant(yPad)));
-            }
-
-            // Set the parent's size.
-            SpringLayout.Constraints pCons = layout.getConstraints(parent);
-            pCons.setConstraint(SpringLayout.SOUTH, y);
-            pCons.setConstraint(SpringLayout.EAST, x);
-        }
-    }
 }
---
+++ src/main/java/gui/user/UpdateReservationPanel.java
@@ -0,0 +1,86 @@
+package gui.user;
+
+import dao.RezervationDAOImp;
+import gui.customcomponents.CardChoosingButton;
+import gui.customcomponents.DatePicker;
+import gui.customcomponents.ErrorLabel;
+import model.Reservation;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/**
+ * Created by marek on 18.5.16.
+ */
+public class UpdateReservationPanel extends JPanel {
+
+    private RezervationDAOImp rezervationDAOImp = new RezervationDAOImp();
+    private final Reservation reservation;
+    private ErrorLabel errorLabel = new ErrorLabel();
+    private DatePicker datePickerFrom = new DatePicker("FROM");
+    private DatePicker datePickerTo = new DatePicker("TO");
+    private JButton makeReservationButton;
+
+    {
+
+    }
+
+    public UpdateReservationPanel(Reservation reservation) {
+        super();
+        this.reservation = reservation;
+        makeReservationButton = new JButton("Update reservation");
+
+        makeReservationButton.addActionListener(e -> {
+            int day = datePickerFrom.getDay();
+            int month = datePickerFrom.getMonth();
+            int year = datePickerFrom.getYear();
+            Calendar c = new GregorianCalendar(year, month, day);
+
+            day = datePickerTo.getDay();
+            month = datePickerTo.getMonth();
+            year = datePickerTo.getYear();
+            Calendar c1 = new GregorianCalendar(year, month, day);
+
+
+            reservation.setFromDate(c);
+            reservation.setToDate(c1);
+
+            rezervationDAOImp.update(reservation);
+
+            new CardChoosingButton("", ReservationManagementPanel.class, true).invoke();
+        });
+
+
+        setupComponents();
+    }
+
+
+    public void setupComponents() {
+        final JPanel mainPanel = addUpperMenu();
+    }
+
+    private JPanel addUpperMenu() {
+        if (reservation == null) {
+            return null;
+        }
+        JPanel mainPanel = new JPanel(new GridLayout(0, 4));
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
+
+        mainPanel.add(datePickerFrom);
+        mainPanel.add(new JLabel());
+        mainPanel.add(datePickerTo);
+        mainPanel.add(new JLabel());
+        add(mainPanel, BorderLayout.PAGE_START);
+
+        JPanel mainBottomPanel = new JPanel(new GridLayout(0, 5));
+        mainBottomPanel.add(new JLabel());
+        mainBottomPanel.add(new JLabel());
+        mainBottomPanel.add(makeReservationButton);
+        mainBottomPanel.add(errorLabel);
+        add(mainBottomPanel, BorderLayout.PAGE_END);
+
+        return mainPanel;
+    }
+}
